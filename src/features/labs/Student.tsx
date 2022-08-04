e10b2e7fd284a06fc4680e8c4933a699df1657ad
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-04 17:58:17 +0700

    175. tsconfig paths

--- src/features/labs/Student.tsx
+++ src/features/labs/Student.tsx
@@ -1,6 +1,6 @@
+import { Student } from '@/models'
 import * as React from 'react'
-import { Student } from '../../models'
-// import { Student } from '../../models/student'
+
 
 export interface StudentCardProps {
   student: Student
