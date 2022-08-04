76d6ce53c9a5eb046a188f86e1df8d281345ebd6
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-04 12:10:18 +0700

    173. Thực hành Props P1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import { Footer, Header } from './components/common'
+import { Student } from './features/labs/Student'
 // import Footer from './components/common/Footer'
 // import Header from './components/common/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function abc() {}
+
   return (
-    <div className="App">
+    <div>
       <Header></Header>
+      <Student
+        name="Easy Frontend"
+        age={18}
+        isHero
+        hobbyList={['eat', 'code', 'sleep']}
+        sayHello={abc}
+      />
       <Footer></Footer>
     </div>
   )
--- src/features/labs/Student.tsx
+++ src/features/labs/Student.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
+import * as React from 'react'
 
 export interface StudentProps {
+  name?: string
+  age: number
+  isHero?: boolean
+  hobbyList: string[]
+  sayHello: () => void
 }
 
-export function Student (props: StudentProps) {
+export function Student({ name = 'Default Name', isHero = false }: StudentProps) {
   return (
     <div>
-      
+      Student: {name} {isHero ? 'hero' : 'no-hero'}
     </div>
-  );
+  )
 }
