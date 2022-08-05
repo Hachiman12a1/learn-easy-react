396f5a1d1fd67736132fbf67630149e9997cb268
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-05 12:00:02 +0700

    188. useState và rules of hooks

---
+++ src/App20.tsx
@@ -0,0 +1,132 @@
+import { Fragment, useEffect, useState } from 'react';
+import './App.css';
+import { Footer, Header, Widget } from './components/common';
+import { MainLayout } from './components/Layout';
+import { MyText } from './features/labs/MyText';
+import { StudentCard } from './features/labs/Student';
+import { Student } from './models/student';
+// import Footer from './components/common/Footer'
+// import Header from './components/common/Header'
+
+function App() {
+  const [count, setCount] = useState(0);
+  // const loading = false;
+
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  function abc() {}
+
+  const john: Student = {
+    name: 'John',
+    age: 3,
+  };
+
+  function handleStudentClick(student: Student) {
+    console.log('student click', student);
+  }
+
+  // if (loading) return <p>Loading.....</p>;
+
+  // server
+  const studentList = [
+    { id: 1, name: 'easy' },
+    { id: 5, name: 'frontend' },
+  ];
+
+  // client
+  const statusList = ['pending', 'active'];
+
+  return (
+    <div>
+      <ul>
+        {studentList.map((student) => (
+          <Fragment key={student.id}>
+            <li>{student.name}</li>
+            <li>{student.name}</li>
+          </Fragment>
+        ))}
+      </ul>
+
+      <ul>
+        {statusList.map((status, idx) => (
+          <li key={idx}>{status}</li>
+        ))}
+      </ul>
+
+      {/* {loading && <p>Loading....</p>}
+      {loading ? <p>Loading....</p> : <p>Data ready!</p>}
+      {loading ? 'Loading....' : 'Data ready!'}
+      <br />
+
+      {true && 'show true'}
+      <br />
+      {false && 'show false'}
+      <br />
+      {'' && 'show empty'}
+      <br />
+
+      {'0' && 'show zero string'}
+      <br />
+
+      {[].length > 0 && 'show zero'}
+      <br />
+
+      {Boolean(NaN) && 'show NaN'}
+      <br />
+
+      <MainLayout>
+        <StudentCard student={john} onClick={handleStudentClick} />
+      </MainLayout>
+
+      <MyText></MyText>
+
+      <MyText>Easy Frontend</MyText>
+      <MyText>{123}</MyText>
+      <MyText>{false}</MyText>
+      <MyText>{null}</MyText>
+      <MyText>{undefined}</MyText>
+
+      <MyText>
+        <span>easy</span>
+      </MyText>
+
+      <MyText>
+        <span>easy</span> frontend
+      </MyText>
+
+      <MyText>
+        <span>easy</span>
+        <span>frontend</span>
+      </MyText>
+
+      <div>
+        <div>
+          <Widget title="Earning Overview">Chart 1</Widget>
+        </div>
+
+        <div>
+          <Widget title="Revenue Sources">
+            <div>Chart 2</div>
+          </Widget>
+        </div>
+
+        <div>
+          <Widget title="Earning Overview">
+            <img src="sdasdda" alt="" />
+          </Widget>
+        </div>
+
+        <div>
+          <Widget title="Earning Overview">Chart 4</Widget>
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
