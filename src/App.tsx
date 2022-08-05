0662d602c5be49e9f7344478ebd01ce6fa45f0ff
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-05 16:05:17 +0700

    190. useState - initializer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 
+// pure
+// impure
+
+let run = 0;
+function calcCount() {
+  run += 5;
+  console.log('calc count', run);
+  return run;
+}
+
 function App() {
   let i = 0;
   const [showMore, setShowMore] = useState(false);
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(calcCount);
   const [name, setName] = useState('');
 
   function handleIncreaseClick() {
-    setCount(count + 1);//async function
-    console.log('after setting count', count);//old count
+    setCount(count + 1); //async function
+    console.log('after setting count', count); //old count
     i += 5;
   }
 
